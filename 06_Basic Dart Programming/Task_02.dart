e899474e07a7d0bef5e19e6ebeafae05700b561d
NdaruDienul <80088755+NdaruDienul@users.noreply.github.com>
2022-11-14 14:23:42 +0700

    Delete 06_Basic Dart Programming directory

--- 06_Basic Dart Programming/Task_02.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  String nama, asal, hobi;
-
-  nama = "Nama saya Ndaru Dienul ";
-  asal = "Saya berasal dari Bogor ";
-  hobi = "Saya memiliki hobi Berenang";
-
-  print(nama + asal + hobi);
-}
